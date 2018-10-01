18e9c784959de4cecf8505801f36376b5f5024e9
Edward Ruchevits <ruchevits@gmail.com>
2018-10-01 12:46:46 +0100

    Add comment to the auth middleware handler

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,8 +1,11 @@
 'use strict'
 
+/**
+ * Authorization middleware.
+ * It does not actually authenticate a user against some database, just appends to the request object
+ * whatever username is specified in the basic authorization header. Password is ignored.
+ */
 module.exports = (req, res, next) => {
-
-  console.log(111)
 
   const authorization = req.headers['authorization']
 
